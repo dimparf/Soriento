a1996040b3be23bda56abcc9668d558aa92d1400
Janos Haber <janos.haber@finesolution.hu>
2015-09-01 10:51:19 +0200

    disable parallell tests

--- src/test/scala/com/emotioncity/soriento/support/OrientDbSupport.scala
+++ src/test/scala/com/emotioncity/soriento/support/OrientDbSupport.scala
@@ -9,6 +9,6 @@ import com.orientechnologies.orient.core.db.OPartitionedDatabasePool
   * Copyright (c) 2014 Dmitriy Parenskiy aka stream (dimparf@gmail.com)
   */
 trait OrientDbSupport {
-  val oDatabaseDocumentPool = new OPartitionedDatabasePool("memory:Test", "admin", "admin").setAutoCreate(true)
+  val oDatabaseDocumentPool = new OPartitionedDatabasePool("memory:Test"+Thread.currentThread().getId, "admin", "admin").setAutoCreate(true)
   implicit val orientDb = oDatabaseDocumentPool.acquire()
 }
