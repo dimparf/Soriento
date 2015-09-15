f7a6a5c2ca7b4a601f8e0f7d74af6cbcfda8653f
Janos Haber <janos.haber@finesolution.hu>
2015-09-16 01:12:56 +0200

    Merge branch 'master' of https://github.com/dimparf/Soriento
    Conflicts:
    	src/main/scala/com/emotioncity/soriento/ReflectionUtils.scala
    	src/test/scala/com/emotioncity/soriento/testmodels/TestModels.scala
    
    Implicit ODocumentReader support

--- src/test/scala/com/emotioncity/soriento/testmodels/Complex.scala
+++ src/test/scala/com/emotioncity/soriento/testmodels/Complex.scala
@@ -10,19 +10,3 @@ import com.orientechnologies.orient.core.record.impl.ODocument
  */
 case class Complex(iField: Int, @Embedded simple: Simple, sField: String, @EmbeddedList listField: List[Simple])
 
-object Complex {
-
-  implicit object ComplexReader extends ODocumentReader[Complex] {
-
-    def read(oDocument: ODocument): Complex = {
-      new Complex(
-        oDocument.get[Int]("iField").get,
-        oDocument.getAs[Simple]("simple").get,
-        oDocument.get[String]("sField").get,
-        oDocument.getAsList[Simple]("listField").get
-      )
-    }
-  }
-
-}
-
--- src/test/scala/com/emotioncity/soriento/testmodels/Message.scala
+++ src/test/scala/com/emotioncity/soriento/testmodels/Message.scala
@@ -8,14 +8,3 @@ import com.orientechnologies.orient.core.record.impl.ODocument
  * Created by stream on 10.08.15.
  */
 case class Message(text: String)
-object Message {
-
-  implicit object MessageReader extends ODocumentReader[Message] {
-
-    def read(oDocument: ODocument): Message = {
-      Message(
-        oDocument.get[String]("text").get
-      )
-    }
-  }
-}
