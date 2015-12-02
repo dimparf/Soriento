dfc76b37aafcfb154a70a47737a792cd1bbc3931
Dmitriy Parenskiy <dmitriy.parenskiy@arsenalmedia.ru>
2015-12-02 11:25:01 +1000

    add async query support tests

---
+++ src/test/scala/com/emotioncity/soriento/RichODatabaseDocumentTest.scala
@@ -0,0 +1,51 @@
+package com.emotioncity.soriento
+
+import javax.persistence.Id
+
+import com.emotioncity.soriento.annotations.{EmbeddedList, Embedded}
+import com.emotioncity.soriento.support.{RemoteOrientDbSupport, OrientDbSupport}
+import com.emotioncity.soriento.testmodels.{ClassWithOptionalRid, Simple, ComplexWithRid}
+import com.orientechnologies.orient.core.id.{ORecordId, ORID}
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.{Inside, BeforeAndAfter, Matchers, FunSuite}
+import RichODocumentImpl._
+import RichODatabaseDocumentImpl._
+
+/**
+  * Created by stream on 02.12.15.
+  */
+class RichODatabaseDocumentTest extends FunSuite
+with Matchers with BeforeAndAfter with Inside with ScalaFutures with Dsl with RemoteOrientDbSupport with ODb {
+  import ODocumentReader._
+
+  test("async query by sql list of models") {
+    createOClass[ClassWithOptionalRid]
+    val mayBeModel1 = ClassWithOptionalRid(name = "My model1").save.as[ClassWithOptionalRid]
+    val mayBeModel2 = ClassWithOptionalRid(name = "My model2").save.as[ClassWithOptionalRid]
+    val mayBeModel3 = ClassWithOptionalRid(name = "My model3").save.as[ClassWithOptionalRid]
+    mayBeModel1 shouldBe defined
+    val model1 = mayBeModel1.get
+    inside(model1) { case ClassWithOptionalRid(rid, name) =>
+        rid shouldBe defined
+        name should equal("My model1")
+    }
+    mayBeModel2 shouldBe defined
+    val model2 = mayBeModel2.get
+    inside(model2) { case ClassWithOptionalRid(rid, name) =>
+      rid shouldBe defined
+      name should equal("My model2")
+    }
+    mayBeModel3 shouldBe defined
+    val model3 = mayBeModel3.get
+    inside(model3) { case ClassWithOptionalRid(rid, name) =>
+      rid shouldBe defined
+      name should equal("My model3")
+    }
+    val savedModelsFuture = orientDb.asyncQueryBySql[ClassWithOptionalRid]("select from ClassWithOptionalRid")
+    whenReady(savedModelsFuture) { savedModels =>
+      savedModels shouldBe a[List[_]]
+      savedModels should have size 3
+    }
+  }
+  dropOClass[ClassWithOptionalRid]
+}
